698882b124309e0ebb0fc279ffa1f1705879c46f
Emic <emic@emic.com>
2016-04-01 13:48:09 -0600

    Agregamos el modelo Productos

--- listacompras/models.py
+++ listacompras/models.py
@@ -1,10 +1,8 @@
 from django.db import models
 from django.utils import timezone
 
-class Post(models.Model):
-    author = models.ForeignKey('auth.User')
-    title = models.CharField(max_length=200)
-    text = models.TextField()
+class Producto(models.Model):
+    Nombre = models.CharField(max_length=200)
     created_date = models.DateTimeField(
             default=timezone.now)
     published_date = models.DateTimeField(
